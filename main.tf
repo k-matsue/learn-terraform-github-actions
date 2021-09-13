a73b48187b98add87c8139759958b51b53bc83fe
Kentaro Matsue <kentaro.matsue@gmail.com>
2021-09-13 18:15:14 +0900

    test 3

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "my-corporation"
+    organization = "k-matsue"
 
     workspaces {
       name = "gh-actions-demo"
